419f2e3dbda5ff1374dbe873ca0d391a07373beb
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-11-16 08:35:30 +0500

    1

---
+++ activity1.py
@@ -0,0 +1,24 @@
+'''What is Computer Programming?
+
+Computer programming means writing instructions (code) that tell a computer what to do. It powers all software, including websites, games, and AI.
+
+ Programming Languages
+
+Programming languages help humans communicate with computers. Examples include Java, C++, HTML, SQL, and Python—each used for different types of development.
+
+Introduction to Python
+
+Python is a simple and versatile language used in web development, AI, data analysis, and more. Created in 1991, it focuses on writing clean and readable code.
+
+Applications of Python
+
+Python is used for:
+
+Games
+
+Machine Learning and AI
+
+Web Development
+
+Data Science and Visualization'''
+
